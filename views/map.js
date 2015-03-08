566acfab274f82eb39764edfdb1d347fca34e86a
Hongjun Choi <hong_jun_choi@brown.edu>
2015-03-07 23:07:45 -0500

    vbef

--- views/map.js
+++ views/map.js
@@ -14,22 +14,7 @@ function initialize(){
 }
 
 google.maps.event.addDomListener(window, "load", initialize);
-
-    $.ajax({
-    type: 'GET',
-    url: "/companies",
-    success: function(data){
-        for(i = 0, len= data.length; i < len; i++){
-            console.log("asdfasdf!")
-          // var newDiv=document.createElement('option');
-          // code = data[i]["code"];
-          // cityName = data[i]["name"]; 
-          // $(newDiv).attr("value",code);
-          // $(newDiv).append(cityName);
-          // $(".region").append(newDiv);
-        }  
-    },
-    statusCode: {
-    401: function() {
-       alert('bad request');
-    }}});
+debugger;
+    $.get('/companies', function(data) {
+        console.log(data);
+    });

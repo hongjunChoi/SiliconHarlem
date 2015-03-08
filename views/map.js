aa40677f7ed3e8b1cef7b01b611d3ce7dcd9e0e1
Hongjun Choi <hong_jun_choi@brown.edu>
2015-03-07 21:25:43 -0500

    asfdasdf

--- views/map.js
+++ views/map.js
@@ -5,8 +5,31 @@ function initialize(){
 		zoom: 11, 
 		center: new google.maps.LatLng(40.85, -73.9)   
 	 }
-
-	 map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+		// var marker = new google.maps.Marker({
+	 //      position: myLatlng,
+	 //      map: map,
+	 //    title: 'Hello World!'
+	 // 	});
+	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 }
 
 google.maps.event.addDomListener(window, "load", initialize);
+
+    // var cityName;
+    // $.ajax({
+    // type: 'GET',
+    // url: "http://",
+    // success: function(data){
+    //     for(i = 0, len= data.length; i < len; i++){
+    //       var newDiv=document.createElement('option');
+    //       code = data[i]["code"];
+    //       cityName = data[i]["name"]; 
+    //       $(newDiv).attr("value",code);
+    //       $(newDiv).append(cityName);
+    //       $(".region").append(newDiv);
+    //     }  
+    // },
+    // statusCode: {
+    // 401: function() {
+    //    alert('bad request');
+    // }}});

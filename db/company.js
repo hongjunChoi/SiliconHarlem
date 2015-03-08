ccb2c61e63f10b1c6007886b08063803f6a85da5
crisscrosskao <crisscrosskao@gmail.com>
2015-03-07 19:26:07 -0500

    changed course to company.

--- db/company.js
+++ db/company.js
@@ -2,13 +2,13 @@ var mongo = require('./mongo');
 
 module.exports = {
   findCompanies: function (callback) {
-    mongo.Course.find(function (err, companies) {
+    mongo.Company.find(function (err, companies) {
       callback(err, companies); //Unknown, find documentation on this#####
     });//implementation of mongodb datastructure########
   }, // change this to find ALL companies.
 
   findCompany: function (_id, callback) {
-    mongo.Course.findById(_id, function (err, company) {
+    mongo.Company.findById(_id, function (err, company) {
       callback(err, company);
     });
   } // change this to find a particular company with the given id.
